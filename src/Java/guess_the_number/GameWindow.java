95d2976b12fda12e1478bbff1e22a703969c1d9e
korotaevilia <ilia.korotaev@domru.ru>
2019-03-13 19:18:42 +0500

    1й урок создай каплю

--- src/Java/guess_the_number/GameWindow.java
+++ src/Java/guess_the_number/GameWindow.java
@@ -1,8 +1,36 @@
 package Java.guess_the_number;
 
-public class GameWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JFrame {
+
+    private static GameWindow game_window;
 
     public static void main(String[] args) {
-	// write your code here
+        game_window = new GameWindow();
+        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game_window.setLocation(200,100);
+        game_window.setSize(906,478);
+        game_window.setResizable(false);
+        GameField game_field = new GameField();
+        game_window.add(game_field);
+        game_window.setVisible(true);
+
+    }
+
+    private static void onRepaint(Graphics g) {
+        g.fillOval(10,10,20,20);
+
+    }
+
+    private static class GameField extends JPanel{
+
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            onRepaint(g);
+
+        }
     }
 }
